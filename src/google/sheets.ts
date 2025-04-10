77f6617c184badcd315931cd91d4cdadc562c806
ludrol <jot.s.gam@gmail.com>
2025-04-11 00:59:14 +0200

    WIP the errors are a bit more sane

--- src/google/sheets.ts
+++ src/google/sheets.ts
@@ -8,6 +8,19 @@ import { google } from 'googleapis';
 
 
 const oauth2Client = new google.auth.OAuth2(process.env.CLIENT_ID, process.env.CLIENT_SECRET, process.env.REDIRECT_URI)
-let access_token = oauth2Client.getAccessToken().then()
+let access_token:string | null | undefined
+oauth2Client.getAccessToken().then((value) => {access_token = value.token}).then(() => {
+	oauth2Client.setCredentials({access_token})
+	console.log(access_token)
+});
 
 
+// oauth2Client.credentials.expiry_date
+
+
+
+
+// oauth2Client.refreshAccessToken((error, token) => {
+//     if (error) {
+//       return
+//     }
