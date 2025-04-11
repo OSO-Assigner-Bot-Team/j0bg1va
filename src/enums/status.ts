d9f5dc953de85b5d0753aaae484195b3633ecb6f
IdfbAn <48176725+IdfbAn@users.noreply.github.com>
2025-04-11 11:30:47 +0400

    Add strings to Status enum

--- src/enums/status.ts
+++ src/enums/status.ts
@@ -1,10 +1,10 @@
 export enum Status {
-    Claimable,
-    MissedDeadline,
-    Dropped,
-    WorkInProgress,
-    AwaitingApproval,
-    UnderRevision,
-    Complete,
-    CreditsNoThread
+    Claimable = 'Claimable',
+    MissedDeadline = 'Missed deadline',
+    Dropped = 'Dropped',
+    WorkInProgress = 'Work in progress',
+    AwaitingApproval = 'Awaiting approval',
+    UnderRevision = 'Under revision',
+    Complete = 'Complete',
+    CreditsNoThread = 'Credits'
 }
