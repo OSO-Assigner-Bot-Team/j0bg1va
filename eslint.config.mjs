583590d348222eb1772278a6100d5ce9eae85f4c
ludrol <jot.s.gam@gmail.com>
2025-04-13 03:53:23 +0200

    removed compilation failure on unused locals tried to add the warning into vscode

---
+++ eslint.config.mjs
@@ -0,0 +1,10 @@
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config({
+	rules: {
+		// Note: you must disable the base rule as it can report incorrect errors
+		'no-unused-vars': 'off',
+		'@typescript-eslint/no-unused-vars': 'error',
+	},
+});
